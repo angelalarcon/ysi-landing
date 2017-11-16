86e013d49a522e413f3a331e1ec167a59321071b
Angel Alarcon <angelalarcon.aa@gmail.com>
2017-11-16 10:10:26 -0400

    responsive gama alta

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('#toggle_options').click(function(){
+		$('section.options').toggleClass("open");
+	});
+
+	var find_it_position = $('#find_it').offset().top;
+	if (($(window).scrollTop() + $(window).height()) < find_it_position) {
+		$('section.options').addClass('sticky');
+	}
+
+	$(window).scroll(function(){
+		if (($(window).scrollTop() + $(window).height()) > find_it_position) {
+			$('section.options').removeClass('sticky');
+		} else {
+			$('section.options').addClass('sticky');
+		}
+	});
+
+});
